1d15273839dde7bbdb881a0bd9185bc50633c062
Ann Nwoko <mtownes@emich.edu>
2023-01-21 22:22:29 -0600

    Completed Assignments

---
+++ index.js
@@ -0,0 +1,49 @@
+const { before } = require("mocha");
+
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
+
+function add(){
+
+}
+function subtract(){
+
+}
+
+function divide(){
+
+}
+
+function multiply(){
+
+}
+
+
+function add(a,b){
+    return (a + b);
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function Incrementn (n){
+    return (n += n);
+}
+
+function Incrementn (n){
+    return n -= n;
+}
